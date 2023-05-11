01e7068493c585d313e31d051ff39917e0e61e0b
Mikayel <mikayel.hovhannisyan27@gmail.com>
2023-05-12 00:15:00 +0400

    feat: setup project and install swagger for documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,17 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'max-len': ['error', 120],
+    'no-unused-vars': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      },
+    ],
   },
 };
---
+++ src/app/constants.ts
@@ -0,0 +1,7 @@
+export const SWAGGER = {
+  TITLE: 'M-One Application',
+  DESCRIPTION: 'M-One API endpoints with parameters',
+  API_VERSION: '1.0',
+  TAG: 'API',
+  DOCUMENTATION_URL: '/docs',
+};
