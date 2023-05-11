c1d05a6df1ee3b56030a7fd4449c2f0d7b7295e8
Mikayel <mikayel.hovhannisyan27@gmail.com>
2023-05-12 00:43:20 +0400

    feat: add globalpipe for validation dtos

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SWAGGER } from './app/constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix('v1');
   const config = new DocumentBuilder()
